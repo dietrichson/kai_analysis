05464189582fdbf6d7fc360b1d15978852684ad3
Aleksander Dietrichson <dietrichson@gmail.com>
2025-03-17 15:43:46 +0000

    cleanup

--- build.R
+++ build.R
@@ -1,4 +1,5 @@
 my_filenam = "kai.analysis_0.1.0_R_x86_64-pc-linux-gnu.tar.gz"
+my_filename = "/Users/sasha/R-tests/kai_analysis/kaianalysis_1.0.1.tar.gz"
 
-rwasm::build("local::/cloud/project/kaianalysis_1.0.1.tar.gz",out_dir = "output/")
+rwasm::add_pkg(paste0("local::", my_filename))
 
